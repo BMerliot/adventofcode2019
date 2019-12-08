d876148b180b144e69fd9aab9763044eb9d9fb59
Baptiste Merliot <baptmer@laposte.net>
2019-12-08 14:48:15 +0100

    day 1

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "adventofcode 2019"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/day01.scala
@@ -0,0 +1,20 @@
+import scala.io.Source
+
+object day01 {
+    def main(args: Array[String]) {
+
+        def computeFuel(n: Int): List[Int] = {
+            val m = n / 3 - 2;
+            if (m < 0) List[Int]()
+            else m :: computeFuel(m)
+        }
+
+        val source = Source.fromFile("src/main/resources/day01.txt")
+        val res = source.getLines
+          .flatMap( (line: String) => computeFuel(line.toInt) )
+          .sum
+        source.close()
+
+        println(res)
+    }
+}
