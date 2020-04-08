f797dab0a9d7ba6c8e3669a18a7b25e26157f51a
Baptiste Merliot <baptmer@laposte.net>
2020-04-09 00:22:38 +0200

    day 16

---
+++ src/main/scala/Day16.scala
@@ -0,0 +1,54 @@
+import scala.io.Source
+
+object Day16 {
+
+  def fft(a: Array[Int]): Array[Int] = {
+
+    a.zipWithIndex.map{
+      case (_, step) => math.abs(
+        a.drop(step).zipWithIndex.filter{
+          case (_, i) => (1 + i / (step + 1)) % 2 == 1
+        }.map{
+          case (x, i) => (1 + i / (step + 1)) % 4 match {
+            case 1 => x
+            case 3 => - x
+          }
+        }.sum
+      ) % 10
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val source = Source.fromFile("src/main/resources/day16.txt")
+    val part1Init: Array[Int] = source.getLines().next()
+        .map(_.toInt - '0').toArray
+    source.close()
+
+    lazy val phraseStream1: LazyList[Array[Int]] =
+      fft(part1Init) #::
+        phraseStream1.map(fft)
+    val part1: String = phraseStream1
+      .take(100).last
+      .take(8)
+      .mkString("")
+
+    println(part1)
+
+    // Thanks again reddit for the trick
+    // the lower half of the array is multiplied by a upper triangular matrix of ones at each fft step
+    // which is equivatent to a partial sum of the following digits
+    val part2Init: Array[Int] =
+      Array.fill(10000)(part1Init).flatten
+    val offSet: Int = part1Init.take(7).foldLeft(0)(_ * 10 + _)
+    println(offSet)
+    val part2: String = (1 to 100).foldLeft(part2Init.drop(offSet).reverse){
+      (a, _) => a.scanLeft(0)(_+_).map(_ % 10)
+    }
+      .reverse
+      .take(8)
+      .mkString
+
+    println(part2)
+  }
+}
